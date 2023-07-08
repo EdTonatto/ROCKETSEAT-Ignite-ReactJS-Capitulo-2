f419fabe48d65e8c1b3d0fd61bcf1647cf1757e3
Eduardo Tonatto <edtonatto@gmail.com>
2023-07-08 17:16:36 -0300

    :sparkles: Added route layouts

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,21 @@
 import { Route, Routes } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/ROCKETSEAT-Ignite-ReactJS-Capitulo-2" element={<Home />} />
-      <Route
-        path="/ROCKETSEAT-Ignite-ReactJS-Capitulo-2/history"
-        element={<History />}
-      />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route
+          path="/ROCKETSEAT-Ignite-ReactJS-Capitulo-2"
+          element={<Home />}
+        />
+        <Route
+          path="/ROCKETSEAT-Ignite-ReactJS-Capitulo-2/history"
+          element={<History />}
+        />
+      </Route>
     </Routes>
   )
 }
