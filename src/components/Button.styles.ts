bedd16b290984b1b9c01c654be5990620f0ad583
Eduardo Tonatto <edtonatto@gmail.com>
2023-07-08 16:30:25 -0300

    :sparkles: Added theme

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import styled, { css } from "styled-components";
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
@@ -16,9 +17,14 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  ${props => {
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
+  /* ${props => {
     return css`
       background-color: ${buttonVariants[props.variant]};
     `;
-  }}
+  }} */
 `
